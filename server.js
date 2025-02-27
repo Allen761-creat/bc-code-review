03b7d9f76efa916b51bb8c2bbe39fa1b2d96d8ff
Allen761-creat <allenanwer69@gmail.com>
2025-02-27 09:08:36 -0800

    Your commit message

--- server.js
+++ server.js
@@ -1,11 +1,29 @@
 
 
-import app from './src/app.js'
+// import app from './src/app.js'
+import express from 'express'
+const app = express()
+import morgan from 'morgan';
+app.use(morgan("dev"));
 
+import bodyParser from 'body-parser';
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(express.json());
+
+import cors from 'cors'
+app.use(cors())
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+import router from './src/Routes/airoute.js'
+app.use('/ai', router)
 const PORT = 1111
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`)
 })
 
+export default app
 
--- src/app.js
+++ src/app.js
@@ -1,20 +1,20 @@
-import express from 'express'
-const app = express()
-import morgan from 'morgan';
-app.use(morgan("dev"));
+// import express from 'express'
+// const app = express()
+// import morgan from 'morgan';
+// app.use(morgan("dev"));
 
-import bodyParser from 'body-parser';
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
-app.use(express.json());
+// import bodyParser from 'body-parser';
+// app.use(bodyParser.urlencoded({ extended: false }));
+// app.use(bodyParser.json());
+// app.use(express.json());
 
-import cors from 'cors'
-app.use(cors())
+// import cors from 'cors'
+// app.use(cors())
 
-app.get('/', (req, res) => {
-    res.send('Hello World!')
-})
-import router from './Routes/airoute.js'
-app.use('/ai', router)
+// app.get('/', (req, res) => {
+//     res.send('Hello World!')
+// })
+// import router from './Routes/airoute.js'
+// app.use('/ai', router)
 
-export default app
+// export default app
